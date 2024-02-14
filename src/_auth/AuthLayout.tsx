84e16aea62d58b88e4adcfc708cf72e0636f6319
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-02-14 23:54:39 +0600

    complate signup ui

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,8 +1,24 @@
-
+import { Outlet, Navigate } from 'react-router-dom';
 
 
 export default function AuthLayout() {
+   const isAuthenticated = false;
    return (
-      <div>AuthLayout</div>
+      <>
+         {isAuthenticated ? (
+            <Navigate to="/" />
+         ) : (
+               <>
+                  <section className='flex flex-1 justify-center items-center flex-col py-10'>
+                     <Outlet />
+                  </section>
+                  <img
+                     src='https://images.pexels.com/photos/2603464/pexels-photo-2603464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+                     className='hidden sm:block h-screen w-1/2 object-cover bg-no-repeat'
+                     alt='image'
+                  />
+               </>
+         )}
+      </>
    );
 }
---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,13 @@
+const Loader = () => (
+   <div className="flex-center w-full">
+      <img
+         src="https://cdn.pixabay.com/photo/2016/08/25/07/30/orange-1618917_1280.png "
+         alt="loader"
+         width={24}
+         height={24}
+         className="animate-spin"
+      />
+   </div>
+);
+ 
+export default Loader;
